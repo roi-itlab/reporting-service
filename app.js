d8043950c6706390c29be2f2de45b027dee27960
Vladislav <actuallyvlad@gmail.com>
2018-10-30 21:37:20 +0300

    Add config support for MongoDB

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
+const fs = require('fs');
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     port = process.env.PORT || 8080;
 
 app.get('/', function(request, response) {
-  var MongoClient = require('mongodb').MongoClient
+  var configJSON = JSON.parse(fs.readFileSync('rs_config.json'));
+  var responseCombined = [];
 
-  MongoClient.connect(
-    'mongodb://localhost:27017/mock_db', 
+  var dbPromises = configJSON.map((entry) => {
+    return new Promise((resolve, reject) => {
+      if (entry.type === "MongoDB") {
+        var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect(
+          entry.config.address + "/" + entry.config.name,
 
-    { useNewUrlParser: true },
+          { useNewUrlParser: true },
 
-    function (err, client) {
-      if (err) { 
-        throw err;
+          function (err, client) {
+            if (err) {
+              reject(err);
+              return;
+            }
+
+            var db = client.db(entry.config.name);
+            var collectionPromises = entry.config.filterList.map((filter) => {
+              return new Promise((resolve, reject) => {
+                db.collection(filter.collection)
+                  .find(filter.query, filter.projection)
+                  .toArray(
+                    function (err, result) {
+                      if (err) {
+                        reject(err);
+                      }
+                      else {
+                        responseCombined.push(result);
+                        resolve();
+                      }
+                  });
+                });
+            });
+
+            Promise.all(collectionPromises)
+              .then(() => resolve())
+              .catch((err) => reject(err));
+          });
       }
-
-      var db = client.db('mock_db')
-      
-      db.collection('currency').find({}, {projection:{_id:0}}).toArray(
-        function (err, result) {
-          if (err) {
-            throw err;
-          }
-
-          response.json(result);
-      });
     });
+  });
+
+  Promise.all(dbPromises)
+          .then(() => response.json(responseCombined))
+          .catch((err) => response.send(err));
 });
 
 server.listen(port, function() {
